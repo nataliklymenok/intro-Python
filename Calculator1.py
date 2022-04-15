d302c4a42768d5d3da0dc45251e2afd0cf075b40
Nataliia Klymenok <nklymenok@gmail.com>
2022-04-15 18:41:35 +0300

    Calculator1 homework

---
+++ Calculator1.py
@@ -0,0 +1,7 @@
+print("Привіт!")
+print("Введіть перше число:")
+input1 = input()
+print("Введіть друге число:")
+input2 = input()
+result = str(int(input1)+int(input2))
+print("Результат: " + result)
