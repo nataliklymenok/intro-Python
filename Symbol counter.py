5a0d41950957a1f12f8e2dd20b7b7cd0c1f9d4bd
Nataliia Klymenok <nklymenok@gmail.com>
2022-05-02 23:13:54 +0300

    Symbol counter homework

---
+++ Symbol counter.py
@@ -0,0 +1,16 @@
+print("Привіт!")
+text = input("Введіть будь-якій текст: ")
+
+text_list = list(text)
+result = dict()
+
+for letter in text_list:
+    count = 0
+    for let in text_list:
+        if letter == let:
+            count += 1
+    result[letter] = count
+
+for key, value in result.items():
+    print(f"Символ '{key}' зустрічається {value} разів")
+
