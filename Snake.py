742bdc5a56a2e698146e563cd3c8113662a16eb7
Nataliia Klymenok <nklymenok@gmail.com>
2022-05-02 22:38:22 +0300

    Snake homework

---
+++ Snake.py
@@ -0,0 +1,15 @@
+import random
+import time
+
+print("Привіт! Зараз будет змійка о_О")
+r = random.randint(5, 50)
+whitespace = " "
+
+while True:
+    try:
+        time.sleep(0.1)
+        print(f"{whitespace * r} *")
+        r = random.randint(r - 1, r + 1)
+    except KeyboardInterrupt:
+        print(f"{whitespace * r} o_O")
+        exit(0)
