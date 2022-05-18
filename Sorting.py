8f04d21b5e61191bff2c2eeab8c8267e2bac18bd
Nataliia Klymenok <nklymenok@gmail.com>
2022-05-18 19:55:51 +0300

    Sorting homework fixed usage

--- Sorting.py
+++ Sorting.py
@@ -4,14 +4,16 @@ randomList = []
 for i in range(0, 10):
     randomList.append(random.randint(0, 100))
 
-print(f"Initial list: {randomList}")
-print(f"Sorted list: {sorted(randomList)}")
-
-
 def sorter(unsorted_list):
     sorted_list = []
     for index in range(0, len(unsorted_list)):
-        min_value = min(unsorted_list[index:])
+        min_value = min(unsorted_list)
         sorted_list.append(min_value)
         unsorted_list.remove(min_value)
     return sorted_list
+
+print(f"Initial list: {randomList}")
+print(f"Sorted list: {sorter(randomList)}")
+
+
+
