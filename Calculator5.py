ecab8eb56ccdc36ea475c8c481ece06e6dc43c7b
Nataliia Klymenok <nklymenok@gmail.com>
2022-06-30 22:45:45 +0300

    Calculator5 homework

--- Calculator5.py
+++ Calculator5.py
@@ -1,85 +1,73 @@
 class Calculator:
-    def __init__(self, operation, operand1, *operand2):
-        self.operation = operation
-        self.operand1 = operand1
-        self.operand2 = operand2
+    def __init__(self, _operation, _operand1, *_operand2):
+        self.operation = _operation
+        self.operand1 = _operand1
+        self.operand2 = _operand2
 
+    def add(self, a, b):
+        return a + b[0]
 
-    def summ(self):
-        return self.operand1 + self.operand2[0]
+    def sub(self, a, b):
+        return a - b[0]
 
+    def mul(self, a, b):
+        return a * b[0]
 
-    def subtr(self):
-        return self.operand1 - self.operand2[0]
-
-
-    def mult(self):
-        return self.operand1 * self.operand2[0]
-
-
-    def divis(self):
+    def div(self, a, b):
         try:
-            divisio = self.operand1 / self.operand2[0]
+            divisio = a / b[0]
         except ZeroDivisionError:
             print("На 0 ділити неможна!")
             divisio = "inf"
         return divisio
 
+    def add_mul(self, a, b):
+        _result = a
+        for element in b:
+            _result += element
+            return _result
 
+    OPERATIONS = {
+        '+': add,
+        '-': sub,
+        '/': div,
+        '*': mul,
+        '+++': add_mul,
+    }
 
-    def calculate(self):
-        result = None
-        if self.operation == "+":
-            result = self.summ()
-
-        elif self.operation == "-":
-            result = self.subtr()
-
-        elif self.operation == "*":
-            result = self.mult()
-
-        elif self.operation == "/":
-            result = self.divis()
-
-
-        elif self.operation == "+++":
-            result = self.operand1
-            for element in self.operand2[0]:
-                result += element
-
+    def calculate(self, _operation, a, b):
+        global result
+        if _operation in self.OPERATIONS.keys():
+            result = self.OPERATIONS[_operation](self, a, b)
         else:
             print("Помилка! Невірна операція!")
             exit()
 
         return result
 
-
-    def printResult(self, result):
+    def print_result(self, _result):
         if self.operation in ('+', '-', '*', '/'):
-            print(f"{self.operand1} {self.operation} {self.operand2[0]} = {result}")
+            print(f"{self.operand1} {self.operation} {self.operand2[0][0]} = {_result}")
         else:
-            prettyString = (' + '.join([str(a) for a in self.operand2[0]]))
-            print(f"{self.operand1} + {prettyString} = {result}")
+            pretty_string = (' + '.join([str(a) for a in self.operand2[0]]))
+            print(f"{self.operand1} + {pretty_string} = {_result}")
 
 
 class Verifier:
 
-
-    def isOperationCommon(self, inp):
+    def is_operation_common(self, inp):
         if inp in ("+", "-", "*", "/"):
             return True
         else:
             return False
 
-
-    def isOperationDifficult(self, inp):
+    def is_operation_difficult(self, inp):
         if inp == "+++":
             return True
         else:
             return False
 
-
-    def isFloat(self, inp):
+    def is_float(self, inp):
         try:
             float(inp)
             return True
@@ -87,45 +75,57 @@ class Verifier:
             return False
 
 
-if __name__ == '__main__':
+class Creator:
     verifier = Verifier()
     operand1 = None
-    operand2 = []
+    operand2 = None
+
+    def create_first_operand(self):
+        global operand1
+        is_number = False
+        while not is_number:
+            operand1 = input("Введіть перше число: ")
+            is_number = verifier.is_float(operand1)
+        return float(operand1)
+
+    def create_second_operand(self):
+        global operand2
+        is_number = False
+        while not is_number:
+            operand2 = input("Введіть друге число: ")
+            is_number = verifier.is_float(operand2)
+        return [float(operand2)]
+
+    def create_multi_operand(self):
+        is_number = True
+        _operand2 = []
+        while is_number:
+            input_val = input("Введіть наступне число: ")
+            if input_val != '':
+                if not verifier.is_float(input_val):
+                    continue
+                _operand2.append(float(input_val))
+            else:
+                is_number = False
+        return _operand2
+
+
+if __name__ == '__main__':
+    verifier = Verifier()
+    create = Creator()
+    operand1 = None
+    operand2 = None
 
     operation = input("Введіть одну з операцій: +, -, *, /, +++: ")
 
-    if verifier.isOperationCommon(operation):
+    if verifier.is_operation_common(operation):
+        operand1 = create.create_first_operand()
+        operand2 = create.create_second_operand()
 
-        isNumber = False
-        while not isNumber:
-            operand1 = input("Введіть перше число: ")
-            isNumber = verifier.isFloat(operand1)
-        operand1 = float(operand1)
-
-        isNumber = False
-        while not isNumber:
-            operand2 = input("Введіть друге число: ")
-            isNumber = verifier.isFloat(operand2)
-        operand2 = float(operand2)
-
-    elif verifier.isOperationDifficult(operation):
-
-        isNumber = False
-        while not isNumber:
-            operand1 = input("Введіть перше число: ")
-            isNumber = verifier.isFloat(operand1)
-        operand1 = float(operand1)
-
-        isNumber = True
-        while isNumber:
-            inputVal = input("Введіть наступне число: ")
-            if inputVal != '':
-                if not verifier.isFloat(inputVal):
-                    continue
-                operand2.append(float(inputVal))
-            else:
-                isNumber = False
+    elif verifier.is_operation_difficult(operation):
+        operand1 = create.create_first_operand()
+        operand2 = create.create_multi_operand()
 
     calc = Calculator(operation, operand1, operand2)
-    result = calc.calculate()
-    calc.printResult(result)
+    result = calc.calculate(operation, operand1, operand2)
+    calc.print_result(result)
